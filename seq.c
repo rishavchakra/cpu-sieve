2951327c2d2b8f8ba3ce0c81080bf6e93b74e0b3
rishavchakra <rishavchakra@gmail.com>
2024-08-31 20:07:26 -0500

    Initial Commit: Memory access patterns

---
+++ seq.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#define BUF_LEN 4096
+#define NUM_SAMPLES 8192 * 4
+
+int main() {
+  // Memory buffer from which we sample
+  uint64_t buf[BUF_LEN];
+
+  // Sample from buffer
+  size_t i;
+  for (i = 0; i < NUM_SAMPLES; ++i) {
+    // TODO: make sure that this actually accesses the memory
+    // access should not be optimized away at any level
+    volatile uint64_t access = buf[i % BUF_LEN];
+  }
+
+  return 0;
+}
