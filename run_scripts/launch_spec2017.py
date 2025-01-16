ae8f52c5cd2f3ae1271099b7fc84388e89d6992b
rishavchakra <rishavchakra@gmail.com>
2025-01-16 17:06:38 -0500

    fix: SPEC + PARSEC scripts

--- run_scripts/launch_spec2017.py
+++ run_scripts/launch_spec2017.py
@@ -5,6 +5,63 @@ from uuid import UUID
 from gem5art.artifact import Artifact
 from gem5art.run import gem5Run
 from gem5art.tasks.tasks import run_gem5_instance
+from gem5art.tasks.tasks import run_job_pool
+
+experiments_repo = Artifact.registerArtifact(
+    # This line may go after `cd gem5-resources`
+    # git checkout 1fe56ffc94005b7fa0ae5634c6edc5e2cb0b7357
+    command="""
+        git clone https://github.com/gem5/gem5-resources
+        cd gem5-resources
+        cd src/spec-2017
+        git init
+        git remote add origin https://github.com/rishavchakra/spec-cpu-tests.git
+    """,
+    typ="git repo",
+    name="spec2017 Experiment",
+    path="./",
+    cwd="./",
+    documentation="""
+        local repo to run spec 2017 experiments with gem5 full system mode;
+        resources cloned from https://github.com/gem5/gem5-resources upto commit 1fe56ffc94005b7fa0ae5634c6edc5e2cb0b7357 of stable branch
+    """,
+)
+
+gem5_repo = Artifact.registerArtifact(
+    command="""
+            git clone -b sieve-research https://github.com/rishavchakra/gem5;
+            cd gem5;
+        """,
+    typ="git repo",
+    name="gem5",
+    path="../gem5/",
+    cwd="./",
+    documentation="cloned gem5 rishav branch from github",
+)
+
+gem5_binary = Artifact.registerArtifact(
+    command="scons build/X86/gem5.opt -j8",
+    typ="gem5 binary",
+    name="gem5",
+    cwd="gem5/",
+    path="gem5/build/X86/gem5.opt",
+    inputs=[
+        gem5_repo,
+    ],
+    documentation="compiled gem5 v20.1.0.4 binary",
+)
+
+m5_binary = Artifact.registerArtifact(
+    command="scons build/x86/out/m5",
+    typ="binary",
+    name="m5",
+    path="gem5/util/m5/build/x86/out/m5",
+    cwd="gem5/util/m5",
+    inputs=[
+        gem5_repo,
+    ],
+    documentation="m5 utility",
+)
 
 packer = Artifact.registerArtifact(
     command="""wget https://releases.hashicorp.com/packer/1.4.3/packer_1.4.3_linux_amd64.zip;
@@ -17,116 +74,18 @@ packer = Artifact.registerArtifact(
     documentation="Program to build disk images. Downloaded sometime in August from hashicorp.",
 )
 
-experiments_repo = Artifact.registerArtifact(
-        # This line may go after `cd gem5-resources`
-        # git checkout 1fe56ffc94005b7fa0ae5634c6edc5e2cb0b7357
-    command = '''
-        git clone https://github.com/gem5/gem5-resources
-        cd gem5-resources
-        cd src/spec-2017
-        git init
-        git remote add origin https://github.com/rishavchakra/spec-cpu-tests.git
-    ''',
-    typ = 'git repo',
-    name = 'spec2017 Experiment',
-    path =  './',
-    cwd = './',
-    documentation = '''
-        local repo to run spec 2017 experiments with gem5 full system mode;
-        resources cloned from https://github.com/gem5/gem5-resources upto commit 1fe56ffc94005b7fa0ae5634c6edc5e2cb0b7357 of stable branch
-    '''
-)
-
-parsec_repo = Artifact.registerArtifact(
-    command="""mkdir parsec-benchmark/;
-    cd parsec-benchmark;
-    git clone https://github.com/darchr/parsec-benchmark.git;""",
-    typ="git repo",
-    name="parsec_repo",
-    path="./disk-image/parsec-benchmark/parsec-benchmark/",
-    cwd="./disk-image/",
-    documentation="main repo to copy parsec source to the disk-image",
-)
-
-gem5_repo = Artifact.registerArtifact(
-        command="""
-            git clone -b sieve-research https://github.com/rishavchakra/gem5;
-            cd gem5;
-        """,
-        typ="git repo",
-        name="gem5",
-        path="../gem5/",
-        cwd="./",
-        documentation="cloned gem5 rishav branch from github"
-)
-
-# gem5_repo = Artifact.registerArtifact(
-    # command="""
-        # git clone https://github.com/gem5/gem5;
-        # cd gem5;
-        # git remote add darchr https://github.com/darchr/gem5;
-        # git fetch darchr;
-        # git cherry-pick 6450aaa7ca9e3040fb9eecf69c51a01884ac370c;
-        # git cherry-pick 3403665994b55f664f4edfc9074650aaa7ddcd2c;
-    # """,
-    # typ="git repo",
-    # name="gem5",
-    # path="../gem5/",
-    # cwd="./",
-    # documentation="cloned gem5 master branch from github (Nov 18, 2019) and cherry-picked 2 commits from darchr/gem5",
-# )
-
-gem5_binary = Artifact.registerArtifact(
-    command="scons build/X86/gem5.opt",
-    typ="gem5 binary",
-    name="gem5",
-    cwd="../gem5/",
-    path="../gem5/build/X86/gem5.opt",
-    inputs=[
-        gem5_repo,
-    ],
-    documentation="gem5 binary",
-)
-
-m5_binary = Artifact.registerArtifact(
-    command="scons build/x86/out/m5",
-    typ="binary",
-    name="m5",
-    path="../gem5/util/m5/m5",
-    cwd="../gem5/util/m5",
-    inputs=[
-        gem5_repo,
-    ],
-    documentation="m5 utility",
-)
-
-'''
-m5_binary = Artifact.registerArtifact(
-    command="make -f Makefile.x86",
-    typ="binary",
-    name="m5",
-    path="../gem5/util/m5/m5",
-    cwd="../gem5/util/m5",
-    inputs=[
-        gem5_repo,
-    ],
-    documentation="m5 utility",
-)
-'''
-
 disk_image = Artifact.registerArtifact(
-    command="./packer build parsec/parsec.json",
+    command="./packer build spec-2017/spec-2017.json",
     typ="disk image",
-    name="parsec",
-    cwd="disk-image",
-    path="disk-image/parsec/parsec-image/parsec",
+    name="spec-2017",
+    cwd="disk-image/",
+    path="disk-image/spec-2017/spec-2017-image/spec-2017",
     inputs=[
         packer,
         experiments_repo,
         m5_binary,
-        parsec_repo,
     ],
-    documentation="Ubuntu with m5 binary and PARSEC installed.",
+    documentation="Ubuntu Server with SPEC 2017 installed, m5 binary installed and root auto login",
 )
 
 linux_repo = Artifact.registerArtifact(
@@ -156,55 +115,93 @@ linux_binary = Artifact.registerArtifact(
     documentation="kernel binary for v4.19.83",
 )
 
+run_scripts_repo = Artifact.registerArtifact(
+    name="run-scripts",
+    typ="git repo",
+
+)
+
 if __name__ == "__main__":
-    num_cpus = ["1"]
+    # cpus = ["kvm", "atomic", "o3", "timing"]
+    cpus = ["kvm"]
+    benchmark_sizes = {
+        "kvm": ["test", "ref"],
+        "atomic": ["test"],
+        "o3": ["test"],
+        "timing": ["test"],
+    }
     benchmarks = [
-        "blackscholes",
-        "bodytrack",
-        "canneal",
-        "dedup",
-        "facesim",
-        "ferret",
-        "fluidanimate",
-        "freqmine",
-        "raytrace",
-        "streamcluster",
-        "swaptions",
-        "vips",
-        "x264",
+        "503.bwaves_r",
+        "507.cactuBSSN_r",
+        "508.namd_r",
+        "510.parest_r",
+        "511.povray_r",
+        "519.lbm_r",
+        "521.wrf_r",
+        "526.blender_r",
+        "527.cam4_r",
+        "538.imagick_r",
+        "544.nab_r",
+        "549.fotonik3d_r",
+        "554.roms_r",
+        "997.specrand_fr",
+        "603.bwaves_s",
+        "607.cactuBSSN_s",
+        "619.lbm_s",
+        "621.wrf_s",
+        "627.cam4_s",
+        "628.pop2_s",
+        "638.imagick_s",
+        "644.nab_s",
+        "649.fotonik3d_s",
+        "654.roms_s",
+        "996.specrand_fs",
+        "500.perlbench_r",
+        "502.gcc_r",
+        "505.mcf_r",
+        "520.omnetpp_r",
+        "523.xalancbmk_r",
+        "525.x264_r",
+        "531.deepsjeng_r",
+        "541.leela_r",
+        "548.exchange2_r",
+        "557.xz_r",
+        "999.specrand_ir",
+        "600.perlbench_s",
+        "602.gcc_s",
+        "605.mcf_s",
+        "620.omnetpp_s",
+        "623.xalancbmk_s",
+        "625.x264_s",
+        "631.deepsjeng_s",
+        "641.leela_s",
+        "648.exchange2_s",
+        "657.xz_s",
+        "998.specrand_is",
     ]
 
-    sizes = ["simsmall"]
-    cpus = ["timing"]
-
+    runs = []
     for cpu in cpus:
-        for num_cpu in num_cpus:
-            for size in sizes:
-                if cpu == "timing" and size != "simsmall":
-                    continue
-                for bm in benchmarks:
-                    run = gem5Run.createFSRun(
-                        "parsec_tests",
-                        "../gem5/build/X86/gem5.opt",
-                        "configs-parsec-tests/run_parsec.py",
-                        f"""results/run_parsec/{bm}/{size}/{cpu}/{num_cpu}""",
-                        gem5_binary,
-                        gem5_repo,
-                        experiments_repo,
-                        "linux-stable/vmlinux-4.19.83",
-                        "disk-image/parsec/parsec-image/parsec",
-                        linux_binary,
-                        disk_image,
-                        cpu,
-                        bm,
-                        size,
-                        num_cpu,
-                        timeout=24 * 60 * 60,  # 24 hours
-                    )
-                    run_gem5_instance.apply_async(
-                        (
-                            run,
-                            os.getcwd(),
-                        )
-                    )
+        for size in benchmark_sizes[cpu]:
+            for benchmark in benchmarks:
+                run = gem5Run.createFSRun(
+                    "gem5 v20.1.0.4 spec 2017 experiment",  # name
+                    "gem5/build/X86/gem5.opt",  # gem5_binary
+                    "gem5-configs/run_spec.py",  # run_script
+                    # relative_outdir
+                    "results/{}/{}/{}".format(cpu, size, benchmark),
+                    gem5_binary,  # gem5_artifact
+                    gem5_repo,  # gem5_git_artifact
+                    run_script_repo,  # run_script_git_artifact
+                    "linux-4.19.83/vmlinux-4.19.83",  # linux_binary
+                    "disk-image/spec2017/spec2017-image/spec2017",  # disk_image
+                    linux_binary,  # linux_binary_artifact
+                    disk_image,  # disk_image_artifact
+                    cpu,
+                    benchmark,
+                    size,  # params
+                    timeout=10 * 24 * 60 * 60,  # 10 days
+                )
+                runs.append(run)
 
+    run_job_pool(runs)
--- run_scripts/parsec.py
+++ run_scripts/parsec.py
@@ -1,6 +1,6 @@
 import subprocess
 from subprocess import Popen
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 import os
 
 benchmarks = [
@@ -22,7 +22,7 @@ benchmarks = [
 replacement_policies = [
     "sieve",
     # "tree-sieve",
-    "lru",
+    # "lru",
     # "fifo",
     "rr",
     # "second-chance",
@@ -32,17 +32,18 @@ replacement_policies = [
 
 assocs = [
     8,
-    4,
     16,
+    4,
     2,
 ]
 
+labels = []
 commands = []
 
 for assoc in assocs:
     for benchmark in benchmarks:
         for repl_policy in replacement_policies:
-            print(
+            labels.append(
                 f"================================\n\
     PARSEC SIMULATION\n\
     Benchmark:     {benchmark}\n\
@@ -50,24 +51,38 @@ for assoc in assocs:
     Associativity:      {assoc}\n"
             )
 
+            # If linux with KVM enabled: switch 'atomic' 7 lines down to 'kvm'
             commands.append(
                 f"""gem5/build/X86/gem5.opt \
 -d out/parsec/{benchmark}/{repl_policy}/{assoc} \
 parsec/configs/run_parsec.py \
 --kernel parsec/vmlinux-4.19.83 \
 --disk parsec/disk-image/parsec/parsec-image/parsec \
---cpu timing \
---size simsmall \
+--cpu atomic \
+--size test \
 --num_cpus 1 \
 --benchmark {benchmark} \
 -a {str(assoc)} \
 -r {repl_policy}"""
             )
 
-cpus = 4
-while commands:
-    batch = commands[:cpus]
-    commands = commands[cpus:]
-    procs = [subprocess.Popen(i, shell=True) for i in batch]
-    for p in procs:
-        p.wait()
+runs = zip(commands, labels)
+
+with ThreadPoolExecutor(max_workers=6) as executor:
+    for run in runs:
+        print(run[1])
+        future = executor.submit(run[0])
+
+# cpus = 4
+# while commands:
+#     run_batch = runs[:cpus]
+#     commands_batch = [run[0] for run in run_batch]
+#     labels_batch = [run[1] for run in run_batch]
+#     runs = runs[cpus:]
+#     # batch = commands[:cpus]
+#     # commands = commands[cpus:]
+#     for label in labels_batch:
+#         print(label)
+#     procs = [subprocess.Popen(cmd, shell=True) for cmd in commands_batch]
+#     for p in procs:
+#         p.wait()
